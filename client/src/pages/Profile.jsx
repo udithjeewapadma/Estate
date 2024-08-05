1812e09860ebf8e79a478bc47d95d03a816c33dd
Jeewapadma <udithjeewapadma117@gmail.com>
2024-08-05 12:39:31 +0530

    complete delete user listings functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -136,6 +136,22 @@ export default function Profile() {
     }
   };
 
+  const handleListingDelete = async (listingId) => {
+    try {
+      const res = await fetch(`/api/listing/delete/${listingId}`,{
+        method: 'DELETE'
+      })
+      const data = await res.json();
+      if(data.success === false){
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) => prev.filter((listing) => listing._id !== listingId));
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
   return (
     <div className='p-2 max-w-md mx-auto'>
       <h1 className='text-3xl font-semibold text-center my-2'>Profile</h1>
@@ -248,7 +264,7 @@ export default function Profile() {
 
               <div className='flex flex-col items-center'>
                 <button className='text-green-700 uppercase'>Edit</button>
-                <button className='text-red-700 uppercase'>Delete</button>
+                <button onClick={()=> handleListingDelete(listing._id)} className='text-red-700 uppercase'>Delete</button>
               </div>
             </div>
           ))}
